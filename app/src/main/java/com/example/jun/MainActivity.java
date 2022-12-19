d9471a8c2cf1834c0fa09984b7f3ede42e741754
Your Name <you@example.com>
2022-12-20 00:53:02 +0900

    tutorial 1

--- app/src/main/java/com/example/jun/MainActivity.java
+++ app/src/main/java/com/example/jun/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.jun;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -27,6 +33,7 @@ import android.widget.TextView;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.PlaceDetectionClient;
 import com.google.android.gms.location.places.PlaceLikelihood;
 import com.google.android.gms.location.places.PlaceLikelihoodBufferResponse;
@@ -53,6 +60,8 @@ public class MainActivity extends AppCompatActivity
 
     EditText searchBox;
     TextView locationText;
+
+    private String TAG = "Tag";
 
     //
     private GoogleMap mMap;       //화면이동, 마커달기 등등으로 쓰이는 구글맵 변수.
@@ -81,6 +90,7 @@ public class MainActivity extends AppCompatActivity
     // 액티비티가 잠시 중지되어서 다시 켜질 때를 위해, 값을 저장할라고 호출되는 onSaveInstanceState()에 사용되는 키값으로, 파라미터로 넘어오는 번들객체에다가
     // 번들의 밸류인 현재좌표, 현재위치정보를 담을 때 사용될 번들의 키값 상수들이다.
 
+    private final LatLng defaultLocation = new LatLng(37.56, 126.97);
 
     // Used for selecting the current place.
     private static final int M_MAX_ENTRIES = 5;    // [메뉴]버튼을 눌렀을 때, 현재위치에서 좋아할만한 장소를 띄울 <최대>갯수로서, 아래 있는 각 장소에 대한 이름/주소/속성/좌표의 최대 갯수가 되기도 한다. 만약 내가 좋아할만한 장소가 int count랑 비교해서 5개가 안되면 count 만큼 띄워지도록 비교하는 문장이 있다.
@@ -189,30 +199,48 @@ public class MainActivity extends AppCompatActivity
         updateLocationUI();
 
         // 8. 핸드폰의 현재위치를 맵의 포지션에 지정하는 매쏘드
-        //getDeviceLocation();
-
-        LatLng SEOUL = new LatLng(37.56, 126.97);
-
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(SEOUL);
-        markerOptions.title("서울");
-        markerOptions.snippet("한국의 수도");
-        mMap.addMarker(markerOptions);
-
-
-        // 기존에 사용하던 다음 2줄은 문제가 있습니다.
-        // CameraUpdateFactory.zoomTo가 오동작하네요.
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
-        //mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 10));
+        getDeviceLocation();
 
     }
 
-    ////////////////////  권한 확인 확인 /////////////////////////
 
-    // 퍼미션 확인하는 메소드
-    private void getLocationPermission() 
-    {
+    ////////////////////  권한 확인 확인 /////////////////////////
+    private void getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (mLocationPermissionGranted) {
+                Task<Location> locationResult = mFusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if (task.isSuccessful()) {
+                            // Set the map's camera position to the current location of the device.
+                            mLastKnownLocation = task.getResult();
+                            if (mLastKnownLocation != null) {
+                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                        new LatLng(mLastKnownLocation.getLatitude(),
+                                                mLastKnownLocation.getLongitude()), DEFAULT_ZOOM));
+                            }
+                        } else {
+                            Log.d(TAG, "Current location is null. Using defaults.");
+                            Log.e(TAG, "Exception: %s", task.getException());
+                            mMap.moveCamera(CameraUpdateFactory
+                                    .newLatLngZoom(defaultLocation, DEFAULT_ZOOM));
+                            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage(), e);
+        }
+    }
+
+    // 퍼미션 확인하는 메소드 (
+    private void getLocationPermission() {
         // 허가 되어 있는 경우 true 할당
         if (ContextCompat.checkSelfPermission(this.getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION)  == PackageManager.PERMISSION_GRANTED)
         {
@@ -242,58 +270,6 @@ public class MainActivity extends AppCompatActivity
         }
         updateLocationUI();
     }
-
-    //////////////////////////////////////////////////////////
-
-    public void mOnClick(View v){
-
-        // 검색창에서 텍스트를 가져온다.
-        String searchText = searchBox.getText().toString();
-        String TAG = "TAG"; String TAG2 = "TAG2"; String TAG3 = "TAG3";
-        Log.d(TAG,searchText);
-        Geocoder geocoder = new Geocoder(getBaseContext());
-        List<Address> addresses = null;
-
-        try {
-            Log.d(TAG2, searchText);
-            // 현재는 영어 검색만 되도록 해둠(v0 버전)
-            addresses = geocoder.getFromLocationName(
-                    String.valueOf(searchText),1);
-
-            if (addresses != null && !addresses.equals(" ")){
-                search(addresses);
-            }
-        } catch (IOException e) {
-            Log.d(TAG2,"err");
-            e.printStackTrace();
-        }
-    };
-
-    // 구글맵 주소 검색 메서도
-    protected void search(List<Address> addresses){
-        Address address = addresses.get(0);
-        LatLng latLng = new LatLng(address.getLatitude(),address.getLongitude());
-
-        String addressText = String.format(
-                "%s, %s",
-                address.getMaxAddressLineIndex() > 0 ? address
-                        .getAddressLine(0) : " ", address.getFeatureName());
-
-        locationText.setVisibility(View.VISIBLE);
-        locationText.setText("Latitude" + address.getLatitude() + "Longitude" + address.getLongitude() + "\n" + addressText);
-
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(latLng);
-        markerOptions.title(addressText);
-
-        mMap.clear();
-        mMap.addMarker(markerOptions);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
-
-    }
-
-
 
     // 자신의 위치로 가는 버튼 표시
     private void updateLocationUI() {
@@ -357,8 +333,13 @@ public class MainActivity extends AppCompatActivity
                     (new OnCompleteListener<PlaceLikelihoodBufferResponse>() {
                         @Override
                         public void onComplete(@NonNull Task<PlaceLikelihoodBufferResponse> task) {
+                            String Tag = "TAG";
+                            Log.d(Tag, String.valueOf(task.isSuccessful()));
+
                             if (task.isSuccessful() && task.getResult() != null) {
                                 PlaceLikelihoodBufferResponse likelyPlaces = task.getResult();
+
+                                Log.d(Tag, String.valueOf(likelyPlaces));
 
                                 // Set the count, handling cases where less than 5 entries are returned.
                                 int count;
@@ -396,13 +377,16 @@ public class MainActivity extends AppCompatActivity
                                 // marker at the selected place.
                                 openPlacesDialog();
 
-                            } else {
+                            }
+                            else {
+                                Log.e(TAG, "Exception: %s", task.getException());
                             }
                         }
                     });
         } else {
             // The user has not granted permission.
-
+            String Tag = "TAG";
+            Log.d(Tag,"error");
             // Add a default marker, because the user hasn't selected a place.
             mMap.addMarker(new MarkerOptions()
                     .title(getString(R.string.default_info_title))
@@ -461,5 +445,60 @@ public class MainActivity extends AppCompatActivity
                 .setItems(mLikelyPlaceNames, listener)
                 .show();
     }
+
+
+    /// custom
+    public void mOnClick(View v){
+
+        // 검색창에서 텍스트를 가져온다.
+        String searchText = searchBox.getText().toString();
+        String TAG = "TAG"; String TAG2 = "TAG2"; String TAG3 = "TAG3";
+        Log.d(TAG,searchText);
+        Geocoder geocoder = new Geocoder(getBaseContext());
+        List<Address> addresses = null;
+
+        try {
+            Log.d(TAG2, searchText);
+            // 현재는 영어 검색만 되도록 해둠(v0 버전)
+            addresses = geocoder.getFromLocationName(
+                    String.valueOf(searchText),1);
+
+            if (addresses != null && !addresses.equals(" ")){
+                search(addresses);
+            }
+        } catch (IOException e) {
+            Log.d(TAG2,"err");
+            e.printStackTrace();
+        }
+    };
+
+    // 구글맵 주소 검색 메서도
+    protected void search(List<Address> addresses){
+        Address address = addresses.get(0);
+        LatLng latLng = new LatLng(address.getLatitude(),address.getLongitude());
+
+        String addressText = String.format(
+                "%s, %s",
+                address.getMaxAddressLineIndex() > 0 ? address
+                        .getAddressLine(0) : " ", address.getFeatureName());
+
+        locationText.setVisibility(View.VISIBLE);
+        locationText.setText("Latitude" + address.getLatitude() + "Longitude" + address.getLongitude() + "\n" + addressText);
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        markerOptions.title(addressText);
+
+        mMap.clear();
+        mMap.addMarker(markerOptions);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////
+
+    
 }
 
